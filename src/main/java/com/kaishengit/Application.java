528df17e819a3466721deb3e2ffee3a1d32da75b
hoyt-wang <hoyt_wang@yahoo.com>
2017-10-30 15:36:19 +0800

    单元测试和手动添加bean

--- src/main/java/com/kaishengit/Application.java
+++ src/main/java/com/kaishengit/Application.java
@@ -1,5 +1,9 @@
 package com.kaishengit;
 
+import com.kaishengit.dao.UserDao;
+import com.kaishengit.service.UserService;
+import com.kaishengit.service.impl.UserServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -13,4 +17,16 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy
 public class Application {
 
+    //手动添加节点
+    /*@Bean
+    public UserDao userDao() {
+        return new UserDao();
+    }
+*/
+   /* @Bean
+    public UserService userService() {
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.setUserDao(userDao());
+        return userService;
+    }*/
 }
--- src/test/java/com/kaishengit/dao/UserDaoTestCase.java
+++ src/test/java/com/kaishengit/dao/UserDaoTestCase.java
@@ -1,19 +1,30 @@
 package com.kaishengit.dao;
 
+import com.kaishengit.Application;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.swing.*;
 
 /**
  * Created by hoyt on 2017/10/30.
  */
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Application.class)
 public class UserDaoTestCase {
 
+    @Autowired
+    private UserDao userDao;
     @Test
     public void saveTest() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserDao userDao = (UserDao) context.getBean("userDao");
+        /*ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserDao userDao = (UserDao) context.getBean("userDao");*/
         userDao.save();
     }
 
